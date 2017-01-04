7f432a7223ac2bd3ef67395a92632dde26d36f23
Eric Kucks <kuckse@pragmatics.com>
2017-01-04 07:59:43 -0500

    num-inversions debugging

--- src/algorithms/algorithms_class.clj
+++ src/algorithms/algorithms_class.clj
@@ -35,35 +35,35 @@
 (defn num-inversions [xs]
   (let [count-split-inv (fn [left-sorted right-sorted]
                           (let [total-count (+ (count left-sorted) (count right-sorted))]
-                            (first (filter (fn [_ _ sorted _] (= (count sorted) (total-count)))
-                                     (iterate (fn [[[left-first & left-rest :as left-sorted]
-                                                    [right-first & right-rest :as right-sorted]
-                                                    sorted
-                                                    count-inversions]]
-                                                (if (<= left-first right-first)
-                                                  [left-rest
-                                                   right-sorted
-                                                   (conj sorted left-first)
-                                                   count-inversions]
-                                                  [left-sorted
-                                                   right-rest
-                                                   (conj sorted right-first)
-                                                   (+ count-inversions (count left-sorted))]))
-                                              [left-sorted right-sorted [] 0])))))
-        num-inversions-sort (fn [xs]
-                              (let [xs-count (count xs)]
+                            (first (filter
+                                    (fn [[_ _ sorted _]] (= (count sorted) total-count))
+                                    (iterate (fn [[[left-first & left-rest :as left-sorted]
+                                                   [right-first & right-rest :as right-sorted]
+                                                   sorted
+                                                   count-inversions]]
+                                               (if (<= left-first right-first)
+                                                 [left-rest
+                                                  right-sorted
+                                                  (conj sorted left-first)
+                                                  count-inversions]
+                                                 [left-sorted
+                                                  right-rest
+                                                  (conj sorted right-first)
+                                                  (+ count-inversions (count left-sorted))]))
+                                             [left-sorted right-sorted [] 0])))))
+        num-inversions-sort (fn num-inversions-sort [xs]
+                              #dbg (let [xs-count (count xs)]
                                 (if (= 1 xs-count)
                                   {:count 0 :sorted xs}
                                   (let [{left-count :count
                                          left-sorted :sorted}
-                                        (num-inversions (take (quot xs-count 2) xs))
+                                        (num-inversions-sort (take (quot xs-count 2) xs))
 
                                         {right-count :count
                                          right-sorted :sorted}
-                                        (num-inversions (drop (quot xs-count 2) xs))
+                                        (num-inversions-sort (drop (quot xs-count 2) xs))
 
-                                        {split-count :count
-                                         split-sorted :sorted}
+                                        [_ _ split-sorted split-count]
                                         (count-split-inv left-sorted right-sorted)]
                                     {:count (+ left-count right-count split-count)
                                      :sorted split-sorted}))))]
