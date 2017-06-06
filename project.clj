69c25abb6df67ff66cfa4dc05c83a286f5e31a02
kuckse <kuckse@pragmatics.com>
2017-06-06 08:02:34 -0400

    clojure 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/math.numeric-tower "0.0.4"] [org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/math.numeric-tower "0.0.4"] [org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot algorithms.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
